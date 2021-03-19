db9cdd36689a8aac8b771a153a0ca9992c29164b
Sophian Akkari <sophian.akkari@mines-paristech.fr>
2021-03-19 19:51:21 +0100

    Scraping des navires de la liste data

--- web-scraping-vessel.py
+++ web-scraping-vessel.py
@@ -3,38 +3,65 @@ import bs4
 from bs4 import BeautifulSoup
 import urllib.request
 import csv
-
-# Ouverture de la page web
-
-URL = 'https://www.vesselfinder.com/fr/vessels/LNG-ALLIANCE-IMO-9320075-MMSI-228333700'
-
-r = urllib.request.Request(URL, headers= {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
-page = urllib.request.urlopen(r)
-soup = BeautifulSoup(page, 'html.parser')
+from datetime import date
+import contextlib
 
 
-# Informations sur le navire
+with open('data.csv', newline='') as csvfile:
+    content = csv.reader(csvfile)
+    for row in content :
+        if row[3] != 'Lien' :
+            try :
+                URL = 'https://www.'+row[3]
 
-section_ship_info = soup.find_all('table', class_='tparams')
-section_ship_info = section_ship_info[2]
+                # Ouverture de la page web
 
-labels = section_ship_info.find_all('td', class_='n3')
-values = section_ship_info.find_all('td', class_='v3')
+                r = urllib.request.Request(URL, headers= {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
 
-# On extrait les valeurs
-# D'abord on prend la date de l'extraction
-update_date = soup.find(id='lastrep')['data-title']
+                with contextlib.closing(urllib.request.urlopen(r)) as page:
+                    soup = BeautifulSoup(page, 'html.parser')
 
-list_labels = ["Date maj"]
-list_values = [update_date]
 
-for label in labels :
-    list_labels.append(label.text)
+                    # Informations sur le navire
 
-for value in values :
-    list_values.append(value.text)
+                    section_ship_info_all = soup.find_all('table', class_='tparams')
+                    section_ship_info = section_ship_info_all[2]
 
-# On écrit dans un fichier csv
-with open('./vessel-info.csv', 'w') as csv_file:
-    csv_writer = csv.writer(csv_file, dialect='excel')
-    csv_writer.writerows(zip(list_labels, list_values))
+                    labels = section_ship_info.find_all('td', class_='n3')
+                    values = section_ship_info.find_all('td', class_='v3')
+
+                    # On extrait les valeurs
+                    # D'abord on prend la date de l'extraction
+                    today = date.today()
+                    update_date = soup.find(id='lastrep')['data-title']
+
+                    list_labels = ["Date scraping", "Date maj"]
+                    list_values = [today, update_date]
+
+                    for label in labels :
+                        list_labels.append(label.text)
+
+                    for value in values :
+                        list_values.append(value.text)
+
+
+                    # On extrait la capacité du navire
+
+                    section_ship_info = section_ship_info_all[3]
+
+                    labels = section_ship_info.find_all('td', class_='n3')
+                    values = section_ship_info.find_all('td', class_='v3')
+
+                    list_labels.append(labels[5].text)
+                    list_labels.append(labels[6].text)
+                    list_values.append(values[5].text)
+                    list_values.append(values[6].text)
+
+                    # On écrit dans un fichier csv
+                    with open(f'./vessel-info-list-test4-{today}.csv', 'a') as csv_file:
+                        csv_writer = csv.writer(csv_file, dialect='excel')
+                        csv_writer.writerows(zip(list_labels, list_values))
+            except IndexError :
+                pass
+            except TypeError :
+                pass
