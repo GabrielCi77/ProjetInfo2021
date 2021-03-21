9cbf9825992d8912a55d14202e50b30fc9ff3c60
sophian-a <72506649+sophian-a@users.noreply.github.com>
2021-03-21 12:01:15 +0100

    Gestion par pandas + données mieux ordonnées

--- web-scraping-vessel.py
+++ web-scraping-vessel.py
@@ -5,7 +5,17 @@ import urllib.request
 import csv
 from datetime import date
 import contextlib
+import pandas as pd
 
+df = pd.DataFrame(columns=["Nom Bateau", "IMO", "MMSI", "Pavillon", "Longueur", "Largeur", "GT", "DWT", "Destination", "ETA", "Tirant d'eau actuel", "Coordonnees", "Direction", "Vitesse", "Date scraping", "Date position"])
+
+def separate_slash(string):
+    i = 0
+    for car in string :
+        if car == '/':
+            ind = i
+        i+=1
+    return string[:ind-1], string[ind+1:]
 
 with open('data.csv', newline='') as csvfile:
     content = csv.reader(csvfile)
@@ -21,47 +31,55 @@ with open('data.csv', newline='') as csvfile:
                 with contextlib.closing(urllib.request.urlopen(r)) as page:
                     soup = BeautifulSoup(page, 'html.parser')
 
-
                     # Informations sur le navire
 
                     section_ship_info_all = soup.find_all('table', class_='tparams')
-                    section_ship_info = section_ship_info_all[2]
-
-                    labels = section_ship_info.find_all('td', class_='n3')
-                    values = section_ship_info.find_all('td', class_='v3')
-
-                    # On extrait les valeurs
-                    # D'abord on prend la date de l'extraction
-                    today = date.today()
-                    update_date = soup.find(id='lastrep')['data-title']
-
-                    list_labels = ["Date scraping", "Date maj"]
-                    list_values = [today, update_date]
-
-                    for label in labels :
-                        list_labels.append(label.text)
-
-                    for value in values :
-                        list_values.append(value.text)
-
-
-                    # On extrait la capacité du navire
-
                     section_ship_info = section_ship_info_all[3]
+                    values1 = section_ship_info.find_all('td', class_='v3')
+                    section_ship_info = section_ship_info_all[2]
+                    values2 = section_ship_info.find_all('td', class_='v3')
 
-                    labels = section_ship_info.find_all('td', class_='n3')
-                    values = section_ship_info.find_all('td', class_='v3')
+                    nom_bateau = values1[1].text
+                    imo = values1[0].text
+                    mmsi = values2[4].text
+                    mmsi = mmsi[10:] # on garde que le mmsi et pas l'imo
+                    pavillon = values1[3].text
+                    longueur = values1[7].text
+                    largeur = values1[8].text
+                    gt = values1[5].text
+                    dwt = values1[6].text
+                    destination = values2[2].text
+                    ETA = values2[3].text # à convertir
+                    tirant = values2[7].text
+                    tirant = tirant[:-2] # il faut enlever l'unité
+                    coordonnees = values2[9].text
+                    direction, vitesse = separate_slash(values2[8].text)
+                    date_position = soup.find(id='lastrep')['data-title']
+                    today = date.today()
 
-                    list_labels.append(labels[5].text)
-                    list_labels.append(labels[6].text)
-                    list_values.append(values[5].text)
-                    list_values.append(values[6].text)
-
-                    # On écrit dans un fichier csv
-                    with open(f'./vessel-info-list-test4-{today}.csv', 'a') as csv_file:
-                        csv_writer = csv.writer(csv_file, dialect='excel')
-                        csv_writer.writerows(zip(list_labels, list_values))
+                    df = df.append({"Nom Bateau" : nom_bateau,
+                                        "IMO": imo,
+                                        "MMSI": mmsi,
+                                        "Pavillon": pavillon,
+                                        "Longueur" : longueur,
+                                        "Largeur" : largeur,
+                                        "GT" : gt,
+                                        "DWT" : dwt,
+                                        "Destination" : destination,
+                                        "ETA" : ETA,
+                                        "Tirant d'eau actuel" : tirant,
+                                        "Coordonnees" : coordonnees,
+                                        "Direction" : direction,
+                                        "Vitesse" : vitesse,
+                                        "Date scraping" : today,
+                                        "Date position" : date_position
+                                        },
+                                        ignore_index=True)
+                    
+                    
             except IndexError :
                 pass
             except TypeError :
                 pass
+
+df.to_csv(f'./vessel-list-{today}.csv', index=False, mode='a')
