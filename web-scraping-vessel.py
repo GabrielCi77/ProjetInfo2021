499a8592031a648bc2c566f787b80fb19f60b72a
sophian-a <72506649+sophian-a@users.noreply.github.com>
2021-05-14 15:13:35 +0200

    Prise en compte com + ajout données
    Pour le fichier de scraping, l'utilisation des DataFrame a été améliorée. Autres améliorations mineures.

--- web-scraping-vessel.py
+++ web-scraping-vessel.py
@@ -7,43 +7,31 @@ from datetime import date
 import contextlib
 import pandas as pd
 
-df = pd.DataFrame(columns=["Nom Bateau", "IMO", "MMSI", "Pavillon", "Longueur", "Largeur", "GT", "DWT", "Origine", "ATD", "Destination", "Destination2", "ETA", "Tirant d'eau actuel", "Coordonnees", "Direction", "Vitesse", "Statut", "Date scraping", "Date position"])
+# df = pd.DataFrame(columns=["Nom Bateau", "IMO", "MMSI", "Pavillon", "Longueur", "Largeur", "GT", "DWT", "Origine", "ATD", "Destination", "Destination2", "ETA", "Tirant d'eau actuel", "Coordonnees", "Direction", "Vitesse", "Statut", "Date scraping", "Date position"])
+# df_columns=["Nom Bateau", "IMO", "MMSI", "Pavillon", "Longueur", "Largeur", "GT", "DWT", "Origine", "ATD", "Destination", "Destination2", "ETA", "Tirant d'eau actuel", "Coordonnees", "Direction", "Vitesse", "Statut", "Date scraping", "Date position"]
 today = date.today()
+list_df = []
 
-'''
-Commentaires E-CUBE (Marwane) : 
-- Reprendre les commentaires des autres scripts
-- separate_slash(string) : remplacer par string.split('/'), fonction native de Python
-- S'il y a des copier-coller, il y a de fortes chances qu'il faille écrire des fonctions supplémentaires 
-    Ex : convertdatewithyear et convertdate sont très similaires, peut-être appeler convertdate dans convertdatewithyear 
-    plutôt que de copier-coller
-'''
 
-def separate_slash(string):
-    i = 0
-    for car in string :
-        if car == '/':
-            ind = i
-        i+=1
-    return string[:ind-1], string[ind+2:]
+def separateSlash(string) :
+    stringlist = string.split('/')
+    word1 = stringlist[0]
+    word2 = stringlist[1]
+    return word1[:-1], word2[1:]
 
-dict_month = {}
-dict_month['Jan']= '01'
-dict_month['Feb']= '02'
-dict_month['Mar']= '03'
-dict_month['Apr']= '04'
-dict_month['May']= '05'
-dict_month['Jun']= '06'
-dict_month['Jul']= '07'
-dict_month['Aug']= '08'
-dict_month['Sep']= '09'
-dict_month['Oct']= '10'
-dict_month['Nov']= '11'
-dict_month['Dec']= '12'
+# On crée un dictionnaire pour remplacer les mois par leur numéro (par exemple Aug par 08)
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+month_numbers = ["%.2d" % i for i in range(1, len(months)+1)]
+dict_month = {months[i]: month_numbers[i] for i in range(len(months))}
 
-def convertdate(string):
-    if string[0] == '-':
-        return '-'
+def convertDate(string) :
+    '''
+    Convertit la date scrapée en format :
+    YYYY-MM-DD-hh:mm
+    '''
+    # Si le string commence par '-' alors il n'y a pas d'info donc on ne fait rien
+    if string[0] == '-' :
+        return '-' # on retourne juste '-' pour supprimer les éventuels \n
     convmonth = dict_month[string[:3]]
     i = 0
     ind = 0
@@ -57,7 +45,7 @@ def convertdate(string):
     convhour = string[ind+2:ind+7]
     return f'2021-{convmonth}-{convday}-{convhour}'
 
-def convertdatewithyear(string):
+def convertDateWithYear(string) :
     if string[0] == '-':
         return '-'
     convmonth = dict_month[string[:3]]
@@ -74,39 +62,39 @@ def convertdatewithyear(string):
     convhour = string[ind+7:ind+12]
     return f'{convyear}-{convmonth}-{convday}-{convhour}'
 
-def extractdestination2(string):
-    if string[0] in ['-', '.', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] :
-        return string
-    else :
-        string = string[2:]
-        dest2 = ''
-        for car in string :
-            if car != ' ':
-                dest2 += car
-        return dest2
+# def extractdestination2(string):
+#     if string[0] in ['-', '.', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] :
+#         return string
+#     else :
+#         string = string[2:]
+#         dest2 = ''
+#         for car in string :
+#             if car != ' ' :
+#                 dest2 += car
+#         return dest2
 
-def extract_destination_eta(string):
+def extractDestinationETA(string) :
     i = 0
     for i in range(len(string)) :
         if string[i:i+4] == "ETA:" or string[i:i+4] == "ATA:" :
             ind = i
         i+=1
     destination = string[1:ind-2]
-    eta = string[ind+5:-2]
+    eta = string[ind+5:-1]
     return destination, eta
 
-def extract_origin_atd(string):
+def extractOriginATD(string) :
     i = 0
     for i in range(len(string)) :
         if string[i:i+4] == "ATD:" or string[i:i+4] == "ATA:" :
             ind = i
         i+=1
     origin = string[1:ind-1]
-    atd = string[ind+5:-1]
+    atd = string[ind+5:-2]
     return origin, atd
 
 
-with open('data.csv', newline='') as csvfile:
+with open('data.csv', newline='') as csvfile :
     content = csv.reader(csvfile)
     for row in content :
         if row[3] != 'Lien' :
@@ -117,7 +105,7 @@ with open('data.csv', newline='') as csvfile:
 
                 r = urllib.request.Request(URL, headers= {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
 
-                with contextlib.closing(urllib.request.urlopen(r)) as page:
+                with contextlib.closing(urllib.request.urlopen(r)) as page :
                     soup = BeautifulSoup(page, 'html.parser')
 
                     # Informations sur le navire
@@ -144,23 +132,23 @@ with open('data.csv', newline='') as csvfile:
                     dwt = values1[6].text
                     origin_brut = soup.find('div', class_= 'vi__r1 vi__stp')
                     origin_brut = origin_brut.text
-                    origin, ATD = extract_origin_atd(origin_brut)
-                    ATD = convertdate(ATD)
+                    origin, ATD = extractOriginATD(origin_brut)
+                    ATD = convertDate(ATD)
                     destination_brut = soup.find('div', class_= 'vi__r1 vi__sbt')
                     destination_brut = destination_brut.text
-                    destination, ETA = extract_destination_eta(destination_brut)
-                    ETA = convertdate(ETA)
+                    destination, ETA = extractDestinationETA(destination_brut)
+                    ETA = convertDate(ETA)
                     # destination2 = value3.text
                     # destination2 = extractdestination2(destination2)
                     tirant = values2[1].text
                     tirant = tirant[:-2] # il faut enlever l'unité
                     # coordonnees = values2[9].text
-                    direction, vitesse = separate_slash(values2[0].text)
+                    direction, vitesse = separateSlash(values2[0].text)
                     statut = values2[2].text
                     date_position = soup.find(id='lastrep')['data-title'] # à convertir
-                    date_position = convertdatewithyear(date_position)
+                    date_position = convertDateWithYear(date_position)
 
-                    df = df.append({"Nom Bateau" : nom_bateau,
+                    list_df.append(pd.DataFrame({"Nom Bateau" : nom_bateau,
                                         "IMO": imo,
                                         "MMSI": mmsi,
                                         "Pavillon": pavillon,
@@ -171,26 +159,25 @@ with open('data.csv', newline='') as csvfile:
                                         "Origine" : origin,
                                         "ATD" : ATD,
                                         "Destination" : destination,
-                                        # "Destination2" : destination2,
+                                        "Destination2" : "",
                                         "ETA" : ETA,
                                         "Tirant d'eau actuel" : tirant,
-                                        # "Coordonnees" : coordonnees,
+                                        "Coordonnees" : "",
                                         "Direction" : direction,
                                         "Vitesse" : vitesse,
                                         "Statut" : statut,
                                         "Date scraping" : today,
                                         "Date position" : date_position
-                                        },
-                                        ignore_index=True)
+                                        }, index = ["IMO"]))
             
-            except AttributeError:
+            except AttributeError :
                 URL = 'https://www.'+row[3]
 
                 # Ouverture de la page web
 
                 r = urllib.request.Request(URL, headers= {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
 
-                with contextlib.closing(urllib.request.urlopen(r)) as page:
+                with contextlib.closing(urllib.request.urlopen(r)) as page :
                     soup = BeautifulSoup(page, 'html.parser')
 
                     # Informations sur le navire
@@ -204,7 +191,7 @@ with open('data.csv', newline='') as csvfile:
 
                         destination_test = soup.find('div', class_= 'vi__r1 vi__sbt')
                         destination_test = destination_test.text
-                        destination, ETA = extract_destination_eta(destination_test)
+                        destination, ETA = extractDestinationETA(destination_test)
 
                         nom_bateau = values1[1].text
                         imo = values1[0].text
@@ -217,19 +204,19 @@ with open('data.csv', newline='') as csvfile:
                         dwt = values1[6].text
                         destination_test = soup.find('div', class_= 'vi__r1 vi__sbt')
                         destination_test = destination_test.text
-                        destination, ETA = extract_destination_eta(destination_test)
-                        ETA = convertdate(ETA)
+                        destination, ETA = extractDestinationETA(destination_test)
+                        ETA = convertDate(ETA)
                         # destination2 = value3.text
                         # destination2 = extractdestination2(destination2)
                         tirant = values2[1].text
                         tirant = tirant[:-2] # il faut enlever l'unité
                         # coordonnees = values2[9].text
-                        direction, vitesse = separate_slash(values2[0].text)
+                        direction, vitesse = separateSlash(values2[0].text)
                         statut = values2[2].text
                         date_position = soup.find(id='lastrep')['data-title'] # à convertir
-                        date_position = convertdatewithyear(date_position)
+                        date_position = convertDateWithYear(date_position)
 
-                        df = df.append({"Nom Bateau" : nom_bateau,
+                        list_df.append(pd.DataFrame({"Nom Bateau" : nom_bateau,
                                             "IMO": imo,
                                             "MMSI": mmsi,
                                             "Pavillon": pavillon,
@@ -250,7 +237,7 @@ with open('data.csv', newline='') as csvfile:
                                             "Date scraping" : today,
                                             "Date position" : date_position
                                             },
-                                            ignore_index=True)
+                                            index = ["IMO"]))
 
                     except IndexError :
                         print("IndexError int")
@@ -262,5 +249,8 @@ with open('data.csv', newline='') as csvfile:
                     print(row[3])
                     pass
 
-
-df.to_csv(f'./list-vessels-{today}.csv', index=False, mode='a')
+# On concatène toutes les dataframes créées
+df = pd.concat(list_df)
+# On crée le csv à partir de la dataframe
+# Index = False évite l'affichage d'une première colonne avec IMO
+df.to_csv(f'./list-vessels-{today}-test.csv', index=False, mode='a')
