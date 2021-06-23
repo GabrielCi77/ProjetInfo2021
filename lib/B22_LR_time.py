aa2bf7c616e8d92848cb6e29eed319d54249b25e
sophian-a <72506649+sophian-a@users.noreply.github.com>
2021-06-23 11:50:39 +0200

    Tests de RL avec KFold + figures

--- lib/B22_LR_time.py
+++ lib/B22_LR_time.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn import model_selection
+from sklearn.model_selection import KFold, cross_validate, GridSearchCV
 from sklearn.linear_model import LinearRegression as LR
 from sklearn import preprocessing
 from sklearn import metrics
@@ -20,78 +20,80 @@ def trainAndPlotAll(df_data):
     X = np.array(df_data.drop(columns=['A_EU']))
     y = np.array(df_data['A_EU'])
 
-    # On sépare en deux jeux de données
+    kf = KFold(n_splits=5, shuffle=True, random_state=13)
 
-    # X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, 
-    #                                                                 test_size=0.20, 
-    #                                                                 random_state=27)
+    scores = []
 
-    size = int(0.2*len(X))
-    X_train = X[0:-size]
-    y_train = y[0:-size]
+    # loop over the different folds
+    for i, (train_index, test_index) in enumerate(kf.split(X)):
+        print("fold: {0} | nb_train: {1} | nb_test: {2}".format(i, len(train_index), len(test_index)))
 
-    X_test = X[-size:]
-    y_test = y[-size:]
+        # get train and validation dataset
+        X_train = X[train_index]
+        X_test = X[test_index]
+        y_train = y[train_index]
+        y_test = y[test_index]
 
-    list_dates_train = list_dates[0:-size]
-    list_dates_test = list_dates[-size:]
+        list_dates_train = list_dates[train_index]
+        list_dates_test = list_dates[test_index]
+        
+        # train model
+        predictor = LR()
+        predictor.fit(X_train, y_train)
+        
+        # measure performance
+        y_pred = predictor.predict(X_test)
+        print("RMSE: %.2f" % metrics.mean_squared_error(y_test, y_pred, squared=False))
+        scores.append(metrics.mean_squared_error(y_test, y_pred, squared=False))
 
-    standard_scaler = preprocessing.StandardScaler()
-    standard_scaler.fit(X_train)
-    X_train = standard_scaler.transform(X_train)
-    X_test = standard_scaler.transform(X_test)
+        # On regarde les coefficients des différentes variables
+        num_features = X_train.shape[1]
+        feature_names = df_data.drop(columns=['A_EU']).columns
+        plt.scatter(range(num_features), np.abs(predictor.coef_))
+        plt.xlabel('Variables')
+        tmp = plt.xticks(range(num_features), feature_names, rotation=90)
+        tmp = plt.ylabel('Coefficients')
+        plt.title("Coefficients de la régression linéaire")
+        plt.savefig(f'../figure/B22_LR_time_coeff_{i}.png')
+        plt.show()
 
-    # On entraine une régression linéaire
-    # print(len(X_train), len(y_train))
-    predictor = LR()
-    predictor.fit(X_train, y_train)
+        # On compare les prédictions aux valeurs réelles
+        fig = plt.figure(figsize=(5, 5))
+        plt.scatter(y_test, y_pred)
 
-    # On la teste sur le jeu de test
-    y_pred = predictor.predict(X_test)
-    print("RMSE: %.2f" % metrics.mean_squared_error(y_test, y_pred, squared=False))
+        plt.xlabel("Nombre réel")
+        plt.ylabel("Nombre prédit")
+        plt.title(f'Régression linéaire : nombre de navires arrivant en Europe (RMSE = {metrics.mean_squared_error(y_test, y_pred, squared=False)})')
 
-    # On regarde les coefficients des différentes variables
-    num_features = X_train.shape[1]
-    feature_names = df_data.drop(columns=['A_EU']).columns
-    plt.scatter(range(num_features), np.abs(predictor.coef_))
-    plt.xlabel('Variables')
-    tmp = plt.xticks(range(num_features), feature_names, rotation=90)
-    tmp = plt.ylabel('Coefficients')
-    plt.title("Coefficients de la régression linéaire")
-    plt.show()
+        # Mêmes valeurs sur les deux axes
+        axis_min = np.min([np.min(y_test), np.min(y_pred)])-1
+        axis_max = np.max([np.max(y_test), np.max(y_pred)])+1
+        plt.xlim(axis_min, axis_max)
+        plt.ylim(axis_min, axis_max)
+        
+        # Diagonale y=x
+        plt.plot([axis_min, axis_max], [axis_min, axis_max], 'k-')
+        plt.savefig(f'../figure/B22_LR_time_diag_{i}.png')
+        plt.show()
 
 
-    fig = plt.figure(figsize=(5, 5))
-    plt.scatter(y_test, y_pred)
+        # Evolution temporelle
+        plt.plot(list_dates, y, 'b')
+        plt.plot(list_dates_test, y_pred, 'r')
+        plt.xlabel("Temps")
+        plt.ylabel("Nombre de navires arrivant en Europe")
+        plt.legend(('Evolution réelle', 'Evolution prédite'), loc='upper right')
+        plt.title("RMSE : %.2f" % metrics.mean_squared_error(y_test, y_pred, squared=False))
+        plt.savefig(f'../figure/B22_LR_time_vesselsperday_{i}.png')
+        plt.show()
 
-    plt.xlabel("Nombre réel")
-    plt.ylabel("Nombre prédit")
-    plt.title(f'Régression linéaire : nombre de navires arrivant en Europe (RMSE = {metrics.mean_squared_error(y_test, y_pred, squared=False)})')
+    print(scores)
 
-    # Mêmes valeurs sur les deux axes
-    axis_min = np.min([np.min(y_test), np.min(y_pred)])-1
-    axis_max = np.max([np.max(y_test), np.max(y_pred)])+1
-    plt.xlim(axis_min, axis_max)
-    plt.ylim(axis_min, axis_max)
-    
-    # Diagonale y=x
-    plt.plot([axis_min, axis_max], [axis_min, axis_max], 'k-')
-    plt.show()
-
-    # Evolution temporelle
-    y_train = list(y_train)
-    y_test = list(y_test)
-    y_tot = y_train + y_test
-    print(len(list_dates), len(y_tot))
-
-    plt.plot(list_dates, y_tot, 'b')
-    plt.plot(list_dates_test, y_pred, 'r')
-    plt.xlabel("Temps")
-    plt.ylabel("Nombre de navires arrivant en Europe")
-    plt.legend(('Réelle', 'Prédite'), loc='upper right')
-    
-    plt.show()
-
+    # performance on the test set
+    average_score = np.mean(scores)
+    std_score = np.std(scores)
+    print("Average RMSE (test): {0:0.3f}".format(average_score))
+    print("Standard deviation RMSE (test): {0:0.3f}".format(std_score), "\n")
 
 
 if __name__ == '__main__':
